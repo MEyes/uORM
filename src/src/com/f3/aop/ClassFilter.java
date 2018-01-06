cf4951485586a18f86cea46d5848255c2f809c94
Eyes <1194679215@qq.com>
2018-01-06 23:20:12 +0800

    PointCut引入

---
+++ src/src/com/f3/aop/ClassFilter.java
@@ -0,0 +1,5 @@
+package com.f3.aop;
+
+public interface ClassFilter {
+	boolean matches(Class targetClass);
+}
---
+++ src/src/com/f3/aop/PintCut.java
@@ -0,0 +1,7 @@
+package com.f3.aop;
+
+public interface PintCut {
+	ClassFilter getClassFilter();
+
+	MethodMatcher getMethodMatcher();
+}
