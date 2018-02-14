26e2f321e8e833203c6cb0d1969cdb9490ab2293
Eyes <1194679215@qq.com>
2018-02-14 23:50:54 +0800

    Contoller和HttpServletBean抽象构建

---
+++ src/com/f3/mvc/Controller.java
@@ -0,0 +1,8 @@
+package com.f3.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+	void handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception;
+}
---
+++ src/com/f3/mvc/HttpServletBean.java
@@ -0,0 +1,8 @@
+package com.f3.mvc;
+
+import javax.servlet.http.HttpServlet;
+
+public abstract class HttpServletBean extends HttpServlet{
+	
+	
+}
